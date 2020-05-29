f54260078398733bf4591de9c5ed72a3229f6e33
satanworker <satanworker@satanworkers-MacBook-Pro.local>
2020-05-29 23:13:24 +0400

    Feature styled-components error

---
+++ src/button/button.tsx
@@ -0,0 +1,8 @@
+import React, { memo, FC } from 'react';
+import styled from 'styled-components';
+
+export const Button: FC<any> = memo(() => {
+  return <Container></Container>;
+});
+
+const Container = styled.button``;
